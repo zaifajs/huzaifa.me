d20d9a5e50e8ff2bacde8bf913be777238e69a14
huzaifaah <huzaifa@brixel.io>
2021-10-14 16:54:24 +0100

    updated

--- src/js/z-main.js
+++ src/js/z-main.js
@@ -22,7 +22,7 @@ var options = {
   cancelOnTouch: true
 };
 
-butter.init(options);
+// butter.init(options);
 
 // Hamburger Menu
 
@@ -47,41 +47,59 @@ ham.addEventListener("keypress", function (e) {
 
 // Select all links with hashes
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function (event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, "") ==
-      this.pathname.replace(/^\//, "") &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $("html, body").animate({
-            scrollTop: target.offset().top
-          },
-          300,
-          function () {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) {
-              // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            }
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function (event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") ==
+    this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate({
+          scrollTop: target.offset().top
+        },
+        300,
+        function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
           }
-        );
-      }
+        }
+      );
     }
-  });
+  }
+});
+
+
+( function() {
+	'use strict';
+
+	if ( 'querySelector' in document && 'addEventListener' in window ) {
+		var body = document.body;
+
+		body.addEventListener( 'mousedown', function() {
+			body.classList.add( 'using-mouse' );
+		} );
+
+		body.addEventListener( 'keydown', function() {
+			body.classList.remove( 'using-mouse' );
+		} );
+	}
+
+} )();
